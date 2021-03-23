b66704abef6f70923918da1a446cc098bb61cae9
generalpask <nils.korsfeldt@gmail.com>
2021-03-23 15:27:15 +0100

    Added symlink setup to installer script and updated readme

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ My configuration for:
 * tmux
 * vim (obsolete), [nvim](https://github.com/neovim/neovim)
 * git
+* irssi and weechat
 
-`.config/` and `.zshenv` should be symlinked into `~`
+`.config/` and `.zshenv` should be symlinked into `~`, the installer script handles this if you wish
 
 A nerdfont (e.g. [Consolas NF](https://github.com/whitecolor/my-nerd-fonts/tree/master/Consolas%20NF)) is recommended for powerlevel10k.
--- install.zsh
+++ install.zsh
@@ -4,14 +4,31 @@
 export XDG_CONFIG_HOME="$HOME/.config"
 
 if [[ ! -f $XDG_CONFIG_HOME/nvim/autoload/plug.vim ]]; then
-    echo -e "\e[36mInstalling vim-plug\e[0m"
-    curl -fLo ${XDG_CONFIG_HOME}/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    echo -e "\e[34mInstalling vim-plug\e[0m"
+    curl -sfLo ${XDG_CONFIG_HOME}/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+else
+    echo -e "\e[33mvim-plug is already installed, skipping\e[0m"
 fi
 
 if [[ ! -d $XDG_CONFIG_HOME/nvim/autoload/plugged ]]; then
-    echo -e "\e[36mInstalling vim plugins\e[0m"
+    echo -e "\e[34mInstalling vim plugins\e[0m"
     nvim -c ':PlugInstall' -c 'qa!'
+else
+    echo -e "\e[33mvim plugins are already installed, skipping\e[0m"
+fi
+
+if [[ ! -d $HOME/.config ]]; then
+    echo -e "\e[34mSymlinking .config to home folder\e[0m"
+    ln -sn $HOME/dotfiles/.config $HOME/.config
+else
+    echo -e "\e[31mCould not symlink .config to home folder, directory already exists\e[0m"
+fi
+
+if [[ ! -f $HOME/.zshenv ]]; then
+    echo -e "\e[34mSymlinking .zshenv to home folder\e[0m"
+    ln -sn $HOME/dotfiles/.zshenv $HOME/.zshenv
+else
+    echo -e "\e[31mCould not symlink .zshenv to home folder, file already exists\e[0m"
 fi
 
 echo -e "\e[32mDone!"
-
